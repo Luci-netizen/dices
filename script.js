2764c72e3c5fb43b8a9976fae0d1a493a8d66b7a
Lucia <lucia@smidova.rocks>
2021-02-11 16:56:50 +0100

    adding files

--- script.js
+++
@@ -1,5 +0,0 @@
-var randomNumber1= Math.random()*6+1;
-randomNumber1=Math.floor(randomNumber1);
-console.log(randomNumber1);
-
-document.querySelectorAll(".img1")[randomNumber1].classList.add("appear");
